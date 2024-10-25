0d4df5207140f39c87397738f635e2be1a7ebf2c
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2024-10-25 14:57:45 +0700

    useEffect

--- src/components/Triplists/index.js
+++ src/components/Triplists/index.js
@@ -3,26 +3,30 @@ import React, { useEffect, useState } from 'react'
 
 export default function Index() {
     let [trips,setTrip] = useState([]);
+    let [url,setUrl] = useState("http://localhost:3000/trips")
 
     useEffect(() => {
-        fetch("http://localhost:3000/trips")
+        fetch(url)
         .then(res=> res.json())
         .then(data => {
             setTrip(data);
         })
-    }, []);
+    }, [url]);
 
     console.log(trips);
     
   return (
     <div>
         <h1>Ready to go?</h1>
+        <button onClick={()=>setUrl("http://localhost:3000/trips")}>All</button>
+        <button onClick={()=>setUrl("http://localhost:3000/trips?location=Myanmar")}>Tirp in Myanmar</button>
         <ul>
             {trips.map(trip=>(
-                <li>
+                <li key={trip.id}>
                     <h3>{trip.name}</h3>
                     <p>price - {trip.price} MMK</p>
-              </li>
+                    <p>location- {trip.location}</p>
+                </li>
             ))}
             
         </ul>
